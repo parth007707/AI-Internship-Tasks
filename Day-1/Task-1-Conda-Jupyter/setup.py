f11bd022c78bc5450a76f11a4709b17fd31ac228
parth007707 <atrishiparth05@gmail.com>
2026-07-08 13:10:17 +0530

    Create setup.py to configure environment with numpy and pandas
    Setup a Python environment with numpy and pandas, and print an array and a DataFrame.

---
+++ Day-1/Task-1-Conda-Jupyter/setup.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+print("Environment Successfully Configured")
+
+arr=np.array([1,2,3,4,5])
+
+print(arr)
+
+df=pd.DataFrame({
+    "Name":["Alice","Bob","Charlie"],
+    "Age":[21,22,23]
+})
+
+print(df)
